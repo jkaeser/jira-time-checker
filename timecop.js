2891ce29080c47aa36e1319094cabcf01603b871
jkaeser <jakaeser44@gmail.com>
2016-10-03 11:53:44 -0400

    Writing better comments, cutting out unnecessary variables, and checking Friday if yesterday was Sunday.

--- timecop.js
+++ timecop.js
@@ -7,7 +7,21 @@ var jirauser = "john";
 var jiraauth = "i7!1a@8KEmzC3CJz";
 var jiraurl  = "https://jira.zivtech.com/";
 
-// Authenticate against Jira.
+/**
+ *  Users included in this array will be ignored by the script. Their time will
+ *  not be checked. They will not receive emails if they do not track all of
+ *  their time.
+ *
+ ********************************
+ *  TO IGNORE A USER:
+ ********************************
+ *   Include the person's Jira username (not email, not full name) in the array
+ *   below. Usernames can be found in the Jira user browser. You need sufficient
+ *   Jira permissions to access this page.
+ *   https://jira.zivtech.com/secure/admin/user/UserBrowser.jspa
+ */
+var ignoreUsers = ['archive', 'sysadmin', 'testguest', 'Alex', 'samantha', 'allie'];
+
 function authenticate() {
   var params = {
     method : "get",
@@ -22,7 +36,7 @@ function authenticate() {
  * Helper function that makes an API call to Jira.
  *
  * @param query
- *  The API endpoint you want to hit with query params.
+ *  The API endpoint you want to hit. Include query parameters.
  */
 
 function callJira(query) {
@@ -40,42 +54,47 @@ function getUsers() {
   return users;
 }
 
+/**
+ * Function returning the previous working day.
+ *
+ * I'm using the term 'yesterday' a bit liberally here. In most cases the
+ * previous working day is the same as yesterday. The only exception is Sunday.
+ */
 function getYesterday() {
   var yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
 
   if (yesterday.getDay() >= 5) {
     yesterday = false;
+  } else if(yesterday.getDay() == 0) {
+    yesterday.setDate(yesterday.getDate() - 2);
   } else {
     // Format to work in Jira filter.
     yesterday = yesterday.toISOString().substr(0,10);
   }
+
   return yesterday;
 }
 
 /**
- * @TODO: Break apart into discrete functions
+ * Function checking tracked time and acting on its findings.
+ *
+ * @TODO: Break apart into discrete functions with one job per function.
  */
 function getTimeTracked() {
   var yesterday = getYesterday();
   if (yesterday) {
-    var users = getUsers();
-
-    // First clean up the sheet from the previous day.
+    // At this point, clear old data out of spreadsheet.
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
     sheet.clearContents();
 
+    var users = getUsers();
     for (i = 0; i < users.length; ++i) {
       var user = users[i];
-      if (user['name'] != 'archive' && user['name'] != 'sysadmin' && user['name'] != 'testguest') {
-
-        // Set variables to make it easier to ask for things.
-        var username = user['name'];
-        var displayName = user['displayName'];
-        var email = user['emailAddress'];
+      if (ignoreUsers.indexOf(user['name']) == -1) {
         var query = "dateFrom=" + yesterday;
         query += "&dateTo=" + yesterday;
-        query += "&username=" + username;
+        query += "&username=" + user['name'];
 
         // Get tracked time. Assume guilty until proven innocent.
         var worklogs = callJira("rest/tempo-timesheets/3/worklogs/?" + query);
@@ -93,12 +112,13 @@ function getTimeTracked() {
           isTimeTracked = 'Yes!';
         }
 
-        // @TODO: don't send emails to certain people. Perhaps build an array up top? Or make configurable in Jira?
         // Print findings to a new row.
-        sheet.appendRow([displayName, totalTime, isTimeTracked]);
-        if (email == 'jkaeser@zivtech.com') {
+        sheet.appendRow([user['displayName'], totalTime, isTimeTracked]);
+
+        // Send email if time not tracked
+        if (user['emailAddress'] == 'jkaeser@zivtech.com') {
           MailApp.sendEmail({
-            to: email,
+            to: user['emailAddress'],
             subject: "You didn't track all your time yesterday!",
             htmlBody: 'Tsk tsk. Go finish up, you sinner.<br /><br />' +
             'You say: <a href="https://jira.zivtech.com/secure/TempoUserBoard!timesheet.jspa">"I\'m so sorry, I\'ll do that right away!"<br />',
