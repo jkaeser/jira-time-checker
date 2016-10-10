9405b094f6e0025e562e48e0e1fd5e2d34757e99
jkaeser <jakaeser44@gmail.com>
2016-10-10 15:23:55 -0400

    Refactoring weekend checks, assorted upgrades.
    Fixed a bug where, if yesterday was Sunday, the range in the Timesheets query would be invalid. This was a result of yesterday getting set to last Friday, but the firstDayInWeek date object was set to today (Monday). I abandoned the notion that the checkTime function would not run on weekends since it was making the code more complex than it needed to be. Now getDates doesn't oddly return a false values if yesterday met certain conditions; instead, it always returns a date and checkTime always completes, though it won't send emails on the weekends.
    
    There are various other upgrades included, such as better comments and pseudo-constant variable names. The spreadsheet that is produced also provides more information, as does the email users will receive if they don't track enough time.

--- timecop.js
+++ timecop.js
@@ -3,9 +3,9 @@
  * https://github.com/chrisurban/jira-sprint-reporting/blob/master/general-query.gs
  */
 
-var jirauser = "jirausername";
-var jiraauth = "seriouslysecurepassword";
-var jiraurl  = "https://jira.example.com/";
+var JIRA_USER = "jirausername";
+var JIRA_AUTH = "seriouslysecurepassword";
+var JIRA_URL = "https://jira.example.com/";
 
 /**
  *  Users included in this array will be ignored by the script. Their time will
@@ -20,140 +20,175 @@ var jiraurl  = "https://jira.example.com/";
  *   Jira permissions to access this page.
  *   https://jira.zivtech.com/secure/admin/user/UserBrowser.jspa
  */
-var ignoreUsers = ['archive', 'sysadmin', 'testguest', 'Alex', 'samantha', 'allie', 'jdelaigle', 'MoGillette'];
+var IGNORE_USERS = ['archive', 'sysadmin', 'testguest', 'Alex', 'samantha',
+                   'allie', 'jdelaigle', 'MoGillette'];
 
 function authenticate() {
-  var params = {
-    method : "get",
-    accept : "application/json",
-      headers: {"Authorization" : "Basic " + Utilities.base64Encode( jirauser + ":" + jiraauth )}
-  };
+    var params = {
+        method: "get",
+        accept: "application/json",
+        headers: {
+            "Authorization": "Basic " + Utilities.base64Encode(JIRA_USER + ":" + JIRA_AUTH)
+        }
+    };
 
-  return params;
+    return params;
 }
 
+
 /**
- * Helper function that makes an API call to Jira.
- *
- * @param query
- *  string; The API endpoint you want to hit. Include query parameters.
+ * @desc Helper function that makes an API call to Jira.
+ * @param query (string)
+ *  The API endpoint you want to hit. Include query parameters.
+ * @return An object of query results.
  */
 function callJira(query) {
-  var data = UrlFetchApp.fetch( jiraurl + query, authenticate() );
-  data = data.getContentText();
-  data = JSON.parse(data);
+    var data = UrlFetchApp.fetch(JIRA_URL + query, authenticate());
+    data = data.getContentText();
+    data = JSON.parse(data);
 
-  return data;
-}
-
-function getUsers() {
-  var users = callJira("rest/api/2/user/search?startAt=0&maxResults=1000&username=zivtech");
-
-  return users;
+    return data;
 }
 
 /**
- * Function returning yesterday's date and the date of the first day of the week.
- *
- * I'm using the term 'yesterday' a bit liberally here. In most cases the
- * previous working day is the same as yesterday. The only exception is Sunday.
+ * @desc Calls Jira to get a list of Zivtech users.
+ * @return An object full of user objects
+ */
+function getUsers() {
+    var users = callJira("rest/api/2/user/search?startAt=0&maxResults=1000&username=zivtech");
+
+    return users;
+}
+
+/**
+ * @desc Function returning yesterday's date and the date of the first day of the week.
+ *  I'm using the term 'yesterday' a bit liberally here. In most cases the
+ *  previous working day is the same as yesterday. The only exception is Sunday.
+ * @return An object full of date objects
  */
 function getDates() {
-  var dates = {
-    yesterday : new Date(),
-    firstDayInWeek : new Date(),
-  };
+    var dates = {
+        yesterday: new Date(),
+        firstDayInWeek: new Date(),
+    };
 
-  dates.yesterday.setDate(dates.yesterday.getDate() - 1);
-  dates.firstDayInWeek.setDate(dates.firstDayInWeek.getDate() - dates.firstDayInWeek.getDay());
+    dates.yesterday.setDate(dates.yesterday.getDate() - 1);
+    dates.firstDayInWeek.setDate(dates.firstDayInWeek.getDate() - dates.firstDayInWeek.getDay());
 
-  if (dates.yesterday.getDay() >= 5) {
-    dates.yesterday = false;
-  } else {
+    // If yesterday was Sunday, get last week's range.
     if (dates.yesterday.getDay() == 0) {
-    dates.yesterday.setDate(dates.yesterday.getDate() - 2);
-    }
-    // Format to work in Jira filter. Consider making a small helper function.
-    dates.yesterday = dates.yesterday.toISOString().substr(0,10);
-    dates.firstDayInWeek = dates.firstDayInWeek.toISOString().substr(0,10);
-  }
+        dates.yesterday.setDate(dates.yesterday.getDate() - 2);
+        dates.firstDayInWeek.setDate(dates.firstDayInWeek.getDate() - 6);
+    };
 
-  return dates;
+    return dates;
 }
 
 /**
- * Fetch a user's worklogs and determine if they've tracked all their time.
- *
- * @param username
- *  string; The Jira username of the person whose time should be fetched.
- * @param dateFrom
- *  string; The date you want to collect worklogs from in yyyy-mm-dd format.
- * @param dateTo
- *  string; The date you want to collect worklogs to in yyyy-mm-dd format.
+ * @desc Converts dates to a string that can be used in a Tempo API query.
+ * @param dates (object)
+ *  An object that includes any number of date objects as properties.
+ * @return An object full of date strings
+ */
+function formatDates(dates) {
+    for (var d in dates) {
+        dates[d] = dates[d].toISOString().substr(0, 10);
+    }
+
+    return dates;
+}
+
+/**
+ * @desc Fetch a user's worklogs and determine if they've tracked all their time.
+ * @param username (string)
+ *  The Jira username of the person whose time should be fetched.
+ * @param dateFrom (string)
+ *  The first date to collect worklogs from in yyyy-mm-dd format.
+ * @param dateTo (string)
+ *  The last date to collect worklogs from in yyyy-mm-dd format.
+ * @return The time worked, in hours, as an integer value
  */
 function getTimeTracked(username, dateFrom, dateTo) {
-  var query = "dateFrom=" + dateFrom;
-  query += "&dateTo=" + dateTo;
-  query += "&username=" + username;
+    var query = "dateFrom=" + dateFrom;
+    query += "&dateTo=" + dateTo;
+    query += "&username=" + username;
 
-  var worklogs = callJira("rest/tempo-timesheets/3/worklogs/?" + query);
-  var time = 0;
+    var worklogs = callJira("rest/tempo-timesheets/3/worklogs/?" + query);
+    var time = 0;
 
-  for (n = 0; n < worklogs.length; ++n) {
-    var worklog = worklogs[n];
-    var worklogTime = worklog['timeSpentSeconds'];
-    time += worklogTime;
-  }
-  time = time / 3600;
+    for (n = 0; n < worklogs.length; ++n) {
+        var worklog = worklogs[n];
+        var worklogTime = worklog['timeSpentSeconds'];
+        time += worklogTime;
+    }
+    time = time / 3600;
 
-  return time;
+    return time;
 }
 
 /**
- * Sends an email reminding a user to track his or her time.
- *
- * @param email
- *  string; The email address to which the email should be sent.
+ * @desc Sends an email reminding a user to track his or her time.
+ * @param email (string)
+ *  The email address to which the email should be sent.
+ * @param dateFirst (string)
+ *  The first date of the range, typically the first day of a week.
+ * @param dateLast (string)
+ *  The last date in the range, typically yesterday or the last day of a week.
+ * @param timeTracked (int)
+ *  The time the user has tracked this week.
+ * @param timeRequired (int)
+ *  The time the user should have tracked by the end of yesterday.
  */
-function sendEmail(email) {
-  MailApp.sendEmail({
-    to: email,
-    subject: "You haven't tracked all your time this week!",
-    htmlBody: 'Tsk tsk. Go finish up, you sinner.<br /><br />' +
-    'You say: <a href="https://jira.zivtech.com/secure/TempoUserBoard!timesheet.jspa">"I\'m so sorry, I\'ll do that right away!"<br />',
-    noReply: true
-  });
+function sendEmail(email, dateFirst, dateLast, timeTracked, timeRequired) {
+    if (email === "jkaeser@zivtech.com") {
+        var timeMissing = timeRequired - timeTracked;
+        MailApp.sendEmail({
+            to: email,
+            subject: "You haven't tracked all your time!",
+            htmlBody: 'You have tracked <strong>' + timeTracked +
+                '</strong> of <strong>' + timeRequired +
+                '</strong> hours so far for the period between ' + dateFirst +
+                ' and ' + dateLast +
+                '. That means you are missing <strong>' + timeMissing +
+                '</strong> hours.<br /><br /> You say: ' +
+                '<a href="https://jira.zivtech.com/secure/TempoUserBoard!timesheet.jspa">' +
+                '"I\'m so sorry, I\'ll do that right away!"</a><br /><br />',
+            noReply: true
+        });
+    }
 }
 
 /**
- * Function checking tracked time and acting on its findings.
+ * @desc Checks employees' tracked time and acts on findings.
  */
 function checkTime() {
-  var dates = getDates();
-  var timeRequired = new Date();
-  timeRequired = timeRequired.getDay() - 1;
-  timeRequired = timeRequired * 7;
-
-  if (dates.yesterday) {
+    var dates = getDates();
+    var timeRequired = 7 * (dates.yesterday.getDay());
+    dates = formatDates(dates);
 
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
     sheet.clearContents();
+    sheet.appendRow(['Who', 'Logged', 'All tracked?', 'Range Start', 'Range End']);
 
     var users = getUsers();
 
     for (i = 0; i < users.length; ++i) {
-      var user = users[i];
-      if (ignoreUsers.indexOf(user['name']) == -1) {
-        var time = getTimeTracked(user['name'], dates.firstDayInWeek, dates.yesterday);
-        if (time >= timeRequired) {
-          sheet.appendRow([user['displayName'], time, 'Yes!']);
-        } else if (time < timeRequired) {
-          sheet.appendRow([user['displayName'], time, 'No']);
-          sendEmail(user['emailAddress']);
-        } else {
-          Logger.log("Could not determine if " + user['displayName'] + " tracked all of his or her time.")
-        };
-      };
-    };
-  };
+        var user = users[i];
+        if (IGNORE_USERS.indexOf(user['name']) == -1) {
+            var time = getTimeTracked(user['name'], dates.firstDayInWeek, dates.yesterday);
+
+            if (time >= timeRequired) {
+                sheet.appendRow([user['displayName'], time, 'Yes!', dates.firstDayInWeek, dates.yesterday]);
+            } else if (time < timeRequired) {
+                sheet.appendRow([user['displayName'], time, 'No', dates.firstDayInWeek, dates.yesterday]);
+                // Don't send emails on weekends.
+                if (dates.yesterday < 5) {
+                    sendEmail(user['emailAddress'], dates.firstDayInWeek, dates.yesterday, time, timeRequired);
+                }
+            } else {
+                sheet.appendRow([user['displayName'], "?", "?", dates.firstDayInWeek, dates.yesterday]);
+                Logger.log("Could not determine if " + user['displayName'] + " tracked all of his or her time.");
+            }
+        }
+    }
 }
